b256eb288861385dd929bd69e8a878893f62d490
lmw216 <lmw216@lehigh.edu>
2014-08-29 15:33:43 +0000

    updated WelcomeClass

---
+++ hw01/WelcomeClass.java
@@ -0,0 +1,30 @@
+//Liam Walsh
+//CSE 2
+// 9-28-2014
+//Welcome Class Java Program
+//
+// first compile the program 
+//      javac WelcomeClass.java
+// run the program
+//      java WelcomeClass//
+
+//Define a Class
+public class WelcomeClass{
+    
+// add main method
+    public static void main(String[] args) {
+
+// Print out lehigh Id design and autobio tweet
+        System.out.println("-----------");
+        System.out.println("| Welcome |");
+        System.out.println("-----------");
+        System.out.println("  ^  ^  ^  ^  ^  ^ ");
+        System.out.println(" / \\/ \\/ \\/ \\/ \\/ \\");
+        System.out.println("|-L--M--W--2--1--6-|");
+        System.out.println(" \\ /\\ /\\ /\\ /\\ /\\ / ");
+        System.out.println("  v  v  v  v  v  v");
+        System.out.println("I am from Atlanta, Georgia. I'm an Industrial and systems engineering major.");
+         
+         //test
+    }
+}
